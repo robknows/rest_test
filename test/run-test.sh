fc2673a03083fcf50821c6ed1bbe34ebb8e4bb8e
Rob <robmoore121@gmail.com>
2018-11-09 00:10:47 +0000

    Moved everything into the __init__ python file

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,2 +1,2 @@
-rest_test_dir=`readlink -f ../rest_test`
+rest_test_dir=`readlink -f ..`
 PYTHONPATH=$PYTHONPATH:$rest_test_dir python3 test.py r
--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 from rest_test import *
