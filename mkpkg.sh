47fec24aed025bd4ea107f9d18f37264a1b8fa92
Rob <robmoore121@gmail.com>
2018-11-16 23:15:39 +0000

    Deploying version 2

--- mkpkg.sh
+++ mkpkg.sh
@@ -6,6 +6,7 @@ sed -i "s/VERSION_NUMBER/$versionNumber.0/g" setup.py
 python3 setup.py sdist
 echo "Created pip package"
 sed -i "s/$versionNumber.0/VERSION_NUMBER/g" setup.py
+git add .
 git commit -am"Deploying version $versionNumber"
 git push
 echo "Pushed version $versionNumber to Github."
