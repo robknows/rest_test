5ef31bb77484013d8d91f36506186487c7e1a483
Rob <robmoore121@gmail.com>
2018-11-16 23:19:31 +0000

    Deploying version 2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -e
 
-versionNumber=$((1 + `cat versionNumber`))
+versionNumber=`cat versionNumber`
 echo "Packaging up version $versionNumber."
 sed -i "s/VERSION_NUMBER/$versionNumber.0/g" setup.py
 python3 setup.py sdist
@@ -10,5 +10,7 @@ git add .
 git commit -am"Deploying version $versionNumber"
 git push
 echo "Pushed version $versionNumber to Github."
+nextVersionNumber="$((1 + $versionNumber)).0"
+echo "Next version number would be $nextVersionNumber"
 
 exit 0
