cd417e29ae3619c3ef1b6a2857bf41e4260ae269
Rob <robmoore121@gmail.com>
2018-11-20 07:52:16 +0000

    Autocommit: Deploying version 3

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ cowsay "Packaging up version $nextVersionNumber."
 sed -i "s/$versionNumber.0/$nextVersionNumber.0/g" setup.py
 python3 setup.py sdist
 cowsay "Created pip package"
+cowsay "Saving new version number '$nextVersionNumber' to file 'versionNumber'"
+rm versionNumber
+echo "$nextVersionNumber" > versionNumber
 git add .
 git commit -am"Autocommit: Deploying version $nextVersionNumber"
 git push
 cowsay "Pushed version $nextVersionNumber to Github."
-cowsay "Saving new version number '$nextVersionNumber' to file 'versionNumber'"
-rm versionNumber
-echo "$nextVersionNumber" > versionNumber
 
 exit 0
